b94b16d15bbde022d9eb7acc2a1cfc4588cc5a6a
Ethan Joiner <ethan.joiner@gmail.com>
2020-01-27 10:42:00 -0600

    post controller complete

---
+++ src/main/java/com/codeup/deimosspringblog/Controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.deimosspringblog.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String index(){
+        return "This is the index page!";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String idPage(@PathVariable long id){
+        return "This is the page for id = " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreatePage(){
+        return "Here is the create page!";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPage(){
+        return "Create a post!";
+    }
+}
