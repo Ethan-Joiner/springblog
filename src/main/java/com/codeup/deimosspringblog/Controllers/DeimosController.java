15e6a86c3e0b55e86e84cb188bb8326f17dc946c
Ethan Joiner <ethan.joiner@gmail.com>
2020-01-27 14:01:34 -0600

    attributes to view

--- src/main/java/com/codeup/deimosspringblog/Controllers/DeimosController.java
+++ src/main/java/com/codeup/deimosspringblog/Controllers/DeimosController.java
@@ -1,6 +1,7 @@
 package com.codeup.deimosspringblog.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DeimosController {
 
-    @GetMapping("/deimos")
+    @GetMapping("/deimos/{days}")
 
-    public String date() {
+    public String date(@PathVariable int days, Model model) {
+        model.addAttribute("days", days);
         return "days";
     }
 }
