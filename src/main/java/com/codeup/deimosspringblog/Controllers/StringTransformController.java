8b78fce1542ce3afe015088900670b52d06e5093
Ethan Joiner <ethan.joiner@gmail.com>
2020-01-27 11:41:52 -0600

    request param exercise complete

--- src/main/java/com/codeup/deimosspringblog/Controllers/StringTransformController.java
+++ src/main/java/com/codeup/deimosspringblog/Controllers/StringTransformController.java
@@ -3,6 +3,7 @@ package com.codeup.deimosspringblog.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,6 +24,28 @@ public class StringTransformController {
     public String upperCase(@PathVariable String toUpper) {
 
         return toUpper.toUpperCase();
+    }
+
+    @GetMapping("/string/both/{string}")
+    @ResponseBody
+    public String both(@PathVariable String string){
+        return upperCase(reverse(string));
+    }
+
+    @GetMapping("/string/{word}")
+    @ResponseBody
+    public String either (@PathVariable String word,
+                          @RequestParam(value="reverse", required=false) boolean reverse,
+                          @RequestParam(value="caps", required=false) boolean caps){
+        if (reverse && caps) {
+            return both(word);
+        } else if (reverse) {
+            return reverse(word);
+        } else if (caps) {
+            return upperCase(word);
+        } else {
+            return word;
+        }
     }
 }
 
