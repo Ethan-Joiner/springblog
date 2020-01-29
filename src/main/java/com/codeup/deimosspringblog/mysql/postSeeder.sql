f227b2fc62aab86afbb6df269ce9dd06a3a50646
Ethan Joiner <ethan.joiner@gmail.com>
2020-01-29 10:49:13 -0600

    step 2 of exercise complete

--- src/main/java/com/codeup/deimosspringblog/mysql/postSeeder.sql
+++ src/main/java/com/codeup/deimosspringblog/mysql/postSeeder.sql
@@ -2,6 +2,9 @@ DROP DATABASE blog_db;
 
 USE blog_db;
 
+INSERT INTO users(id, username, email, password)
+VALUES (1, 'User1', 'test@email.com', 'password');
+
 INSERT INTO posts (title, body)
 VALUES
 ( 'Test1', 'This  is  the body for Test1'),
